3b6c14d1fa58a666ca03b048a52cb9f41180c114
speech <speech@ke.com>
2023-02-14 10:55:14 +0800

    add ubuntu 20.04 suppoer

--- install_env.sh
+++ install_env.sh
@@ -10,6 +10,9 @@ sudo apt install -y build-essential automake autoconf libtool flex bison libboos
 sudo apt-get install -y  bison libbison-dev
 sudo apt update
 sudo apt install -y build-essential qt5-default libfontconfig1-dev libaudio-dev libxt-dev libglib2.0-dev libxi-dev libxrender-dev
+
+sudo apt-get install -y qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools # for ubuntu20.04
+
 sudo apt install -y git
 
 
